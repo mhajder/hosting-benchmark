4dd5974dc4925f4fe16fd253ff785a26a6a9c657
Mateusz Hajder <mdhm@wp.pl>
2020-10-03 14:12:11 +0200

    Update end-line character in mysql_info.php

--- hosting_benchmark/server/api/mysql_info.php
+++ hosting_benchmark/server/api/mysql_info.php
@@ -1,1 +1,33 @@
-<?phpheader('Content-type: application/json');include '../inc.php';set_time_limit(60 * 2);function getMysqliObject() {    $mysqli = new mysqli($GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname']);    if ($mysqli->connect_error) {        die('Could not connect to database');    }    return $mysqli;}// https://www.php.net/manual/en/mysqli.get-server-info.php#118822function getServerInfo() {    $mysqli = getMysqliObject();    $query = "SELECT version();";    $stmt = $mysqli->prepare($query);    $stmt->execute();    $result = $stmt->get_result();    $version = $result->fetch_assoc()['version()'];    $mysqli->close();    return $version;}$results = array(    "serverInfo" => getServerInfo(),    "clientInfo" => mysqli_get_client_info(),);echo json_encode($results);
+<?php
+header('Content-type: application/json');
+
+include '../inc.php';
+
+set_time_limit(60 * 2);
+
+function getMysqliObject() {
+    $mysqli = new mysqli($GLOBALS['dbhost'], $GLOBALS['dbuser'], $GLOBALS['dbpass'], $GLOBALS['dbname']);
+    if ($mysqli->connect_error) {
+        die('Could not connect to database');
+    }
+    return $mysqli;
+}
+
+// https://www.php.net/manual/en/mysqli.get-server-info.php#118822
+function getServerInfo() {
+    $mysqli = getMysqliObject();
+    $query = "SELECT version();";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $version = $result->fetch_assoc()['version()'];
+    $mysqli->close();
+    return $version;
+}
+
+$results = array(
+    "serverInfo" => getServerInfo(),
+    "clientInfo" => mysqli_get_client_info(),
+);
+
+echo json_encode($results);
